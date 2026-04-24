3ffd08a076300de9c4e5a33d6db950244de7c8b7
a25alebrados <a25alebrados@inspedralbes.cat>
2026-04-24 13:29:42 +0200

    Connecxio i index php

--- php/index.php
+++ php/index.php
@@ -1,1 +1,18 @@
-HOLA!!!
+<?php
+    $titulo = "Benvingut al registre d'incidències de l'Institut Pedralbes";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo; ?></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <img src="https://www.institutpedralbes.cat/wp-content/uploads/2021/05/logo.jpg" alt="Logo Institut Pedralbes">   
+    
+</body>
+</html>
