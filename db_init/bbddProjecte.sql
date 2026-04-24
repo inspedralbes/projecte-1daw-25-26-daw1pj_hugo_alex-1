de2c478249b446c9ac25588e566d9dba604d11fa
a25alebrados <a25alebrados@inspedralbes.cat>
2026-04-24 12:10:59 +0200

    actualiztcion db

--- db_init/bbddProjecte.sql
+++ db_init/bbddProjecte.sql
@@ -12,18 +12,18 @@ USE indidenciesP;
 -- 1. DEPARTAMENTO
 CREATE TABLE DEPARTAMENTO (
     idDepartamento INT (11) AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR (200)
+    nombre VARCHAR (200) NOT NULL
 );
 -- 2. TECNICO
 CREATE TABLE TECNICO (
     idTecnico INT (11) AUTO_INCREMENT PRIMARY KEY, 
-    nombre VARCHAR (200)
+    nombre VARCHAR (200) NOT NULL
 );
 
 -- 3. TIPO
 CREATE TABLE TIPO (
     idTipo INT (7) AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR (25)
+    nombre VARCHAR (25) NOT NULL
 );
 
 -- 4. INCIDENCIA
@@ -32,10 +32,10 @@ CREATE TABLE INCIDENCIA (
     idTecnico INT (11),
     idDepartamento INT (11),
     idTipo INT (7),
-    fechaInicio TIMESTAMP,
+    fechaInicio TIMESTAMP NOT NULL,
     fechaFin DATE,
     descripcion VARCHAR (2000),
-    prioritat ENUM('Alta', 'Mitja', 'Baixa'),
+    prioritat ENUM('Alta', 'Mitja', 'Baixa') NOT NULL,
     FOREIGN KEY (idTecnico) REFERENCES TECNICO(idTecnico),
     FOREIGN KEY (idTipo) REFERENCES TIPO(idTipo),
     FOREIGN KEY (idDepartamento) REFERENCES DEPARTAMENTO (idDepartamento)
@@ -45,8 +45,8 @@ CREATE TABLE ACCION (
     idAccion INT (11) AUTO_INCREMENT PRIMARY KEY,
     idIncidencia INT (11),
     comentario VARCHAR (2000),
-    tiempo INT (3),
-    fechaAccion TIMESTAMP,
-    visible INT (1),
+    tiempo INT (3) NOT NULL,
+    fechaAccion TIMESTAMP NOT NULL,
+    visible INT (1) NOT NULL,
     FOREIGN KEY (idIncidencia) REFERENCES INCIDENCIA (idIncidencia)
 );
