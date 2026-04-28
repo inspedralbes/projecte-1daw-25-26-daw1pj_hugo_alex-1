bb86832ef1344f8778e9816b47e9ec2d42ad53a4
a25hugberbat <a25hugberbat@inspedralbes.cat>
2026-04-28 14:12:35 +0200

    Header y fotter + crear admin.php y tecnic.php

---
+++ php/fotter.php
@@ -0,0 +1,11 @@
+</main><footer class="bg-light text-center border-top py-4 mt-5">
+    <div class="container">
+        <p class="mb-1">© 2026 Institut Pedralbes</p>
+        <div class="small">
+            <h7 href="#" class="text-secondary me-3 text-decoration-none">Hugo Berea</h7>
+            <h7 href="#" class="text-secondary text-decoration-none">Alexander Brandao</h7>
+        </div>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</html>
--- php/index.php
+++ php/index.php
@@ -1,7 +1,13 @@
-<?php
-    include_once "header.php";
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <main class="flex-grow-1">
 <div class="row justify-content-center mt-5">
     <div class="col-md-8 text-center">
         <div class="p-5 mb-4 bg-light rounded-3 shadow-lg border-primary" style="border-top: 5px solid #0d6efd;">
@@ -17,18 +23,22 @@
                 </p>
                 <hr class="my-4">
                 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <a href="user.php" class="btn btn-primary btn-lg px-5 shadow">
-                        <i class="bi bi-collection-play"></i> Usuari
+                    <a href="crear_incidencia.php" class="btn btn-primary btn-lg px-5 shadow">
+                        <div bclass="bi bi-collection-play">Usuari</div>
                     </a>
                     <a href="tecnic.php" class="btn btn-primary btn-lg px-5 shadow">
-                        <i class="bi bi-plus-circle"></i> Técnic
+                        <div class="bi bi-plus-circle">Técnic</div>
                     </a>
                     <a href="admin.php" class="btn btn-primary btn-lg px-5 shadow">
-                        <i class="bi bi-collection-play"></i> Admin
+                        <div class="bi bi-collection-play">Admin</div>
                     </a>
                 </div>
             </div>
         </div>
     </div>
+</main>
+<?php
+    include_once "fotter.php"
+?>
 </body>
 </html>
