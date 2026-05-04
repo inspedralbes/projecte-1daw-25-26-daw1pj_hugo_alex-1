9c328b7acb937824ea718cdaae8ff5071788140e
a25hugberbat <a25hugberbat@inspedralbes.cat>
2026-05-04 12:00:24 +0200

    crear inci

--- php/crear_incidencia.php
+++ php/crear_incidencia.php
@@ -1,31 +1,16 @@
 <?php
 include_once "header.php";
 ?>
-<?php
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-include_once "connexio.php";
-
-$idTipus = $_POST["idTipus"];
-$idDepartaments = $_POST["idDepartament"];
-$descripcio = $_POST["descripcio"];
-
-$sentencia = $conn->prepare("INSERT INTO INCIDENCIA (idTipo, idDepartamento, fechaInicio, descripcion) VALUES(?, ?, NOW() ,?)");
-$sentencia->bind_param("iis", $idTipus, $idDepartaments, $descripcio);
-$sentencia->execute();
-header("Location: llistar_incidencies_usuari.php");
 
 
-?>
+
+<a href="llistar_incidencies_usuari.php" class="btn btn-primary btn-lg px-5 shadow">
+                        <div bclass="bi bi-collection-play">prov</div>
+                    </a>
+
+
+
 
 <?php
 include_once "fotter.php";
 ?>
-
-
-
-
-
-
-
-
-
