1035b2b2c38b1a60fdacd58965901d4f475c4d16
a25hugberbat <a25hugberbat@inspedralbes.cat>
2026-05-11 11:39:29 +0200

    connexion

--- php/connexio.php
+++ php/connexio.php
@@ -1,22 +1,20 @@
 <?php
-if (getenv('VAR1')) {
-    // Estamos en Docker (local)
+$enDocker = (gethostbyname('db') !== 'db');
+
+if ($enDocker) {
     $servername = "db";
     $username = getenv('VAR1');
     $password = getenv('VAR2');
     $dbname = "indidenciesP";
 } else {
-    // Estamos en Hestia (servidor real)
-    $servername = "localhost"; 
-    $username = "a25hugberbat_incidencies";    
-    $password = "Plataforma_Incidencies1234";        
+    $servername = "localhost";
+    $username = "a25hugberbat_incidencies";
+    $password = "Plataforma_Incidencies1234";
     $dbname = "a25hugberbat_incidencies";
 }
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
-    echo "<p>Error de connexió: " . htmlspecialchars($conn->connect_error) . "</p>";
     die("Error de connexió: " . $conn->connect_error);
 }
 ?>
